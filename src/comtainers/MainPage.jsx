acac70b34ab77739aaf7a2e56870516d0b2f7dcc
Shaun <shaun31415926@gmail.com>
2023-04-11 13:30:47 +0800

    error in routing4

--- src/comtainers/MainPage.jsx
+++ src/comtainers/MainPage.jsx
@@ -7,7 +7,7 @@ export default function MainPage() {
       <img class="rounded-full h-[360px] w-[493px]" src={didiImage}/>
       <p class='text-7xl mt-2 font-medium'>Welcome!</p>
       <p class=' text-3xl mt-3 text-gray-900/50'>這是我Lab05實驗的網站</p>
-      <a href="/about" className="rounded-full m-5 px-3 py-1 text-xl  font-semibold leading-6 text-gray-600 ring-2 ring-indigo-500/30 hover:ring-indigo-500/50 ">
+      <a href="/Lab05/about" className="rounded-full m-5 px-3 py-1 text-xl  font-semibold leading-6 text-gray-600 ring-2 ring-indigo-500/30 hover:ring-indigo-500/50 ">
         關於我<span aria-hidden="true">&rarr;</span>
       </a>
     </div>
